91932b8f090444ab1c6304b368f467d43d81060a
Jullia Santos Ross <141286753+JulliaRoss@users.noreply.github.com>
2023-09-28 22:18:17 -0300

    Atualizei a classe usuario
    Adicionei a variavel identificador e removi os Sets inutilizaveis.

--- Usuario.java
+++ Usuario.java
@@ -1,14 +1,13 @@
 public class Usuario {
     protected String nome;
 
+    protected int identificador;
+
     protected String tipos;
 
-    Public Usuario() {
-
-    }
-
-    Public Usuario(String nome, String tipos) {
+    public Usuario(String nome, int identificador, String tipos) {
         this.nome = nome;
+        this.identificador = identificador;
         this.tipos = tipos;
     }
 
@@ -16,15 +15,11 @@ public class Usuario {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public int getIdentificador() {
+        return identificador;
     }
 
     public String getTipos() {
         return tipos;
-    }
-
-    public void setTipos(String tipos) {
-        this.tipos = tipos;
     }
 }
