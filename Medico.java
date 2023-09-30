3eee77f370a571a36fca67fe64cbdab8a2f5892e
Leticia Davi <93017408+leticiadavinunes@users.noreply.github.com>
2023-09-30 17:16:27 -0300

    Adicionaei a classe app
    Ela serve com a visão do usuário e suas possíveis ações.

--- Medico.java
+++ Medico.java
@@ -1,7 +1,39 @@
-public class Medico extends Usuario {
-    //Autorizacao autorizacao;
+public class Autorizacao {
+    private int codigo;
 
-    public Medico (String nome, String tipo,int indentificador){
-        super.Usuario();
+    private String data;
+
+    private String exame;
+
+    private String paciente;
+
+    private Medico medico;
+
+    public Autorizacao(int codigo, String data, String exame, String paciente, Medico medico) {
+        this.codigo = codigo;
+        this.data = data;
+        this.exame = exame;
+        this.paciente = paciente;
+        this.medico = medico;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getExame() {
+        return exame;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
     }
 }
